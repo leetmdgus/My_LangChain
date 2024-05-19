53b525ebd76547566b044f0826bb6bfd1d39d686
leetmdgus <lee_tmdgus@naver.com>
2024-05-19 09:03:55 +0900

    add Langchain folder

---
+++ LangChain/langchain/document_loaders/DirectoryLoader.py
@@ -0,0 +1,7 @@
+#DirectoryLoader는 폴더 안에 존재하는 파일들을 필터링해서 읽어옵니다.
+
+class DirectoryLoader:
+    def __init__(self, path, glob, loader_cls):
+        pass
+    def load():
+        pass
---
+++ LangChain/langchain/document_loaders/UnstructuredHTMLLoader.py
@@ -0,0 +1,8 @@
+#HTML
+#UnstructedHTMLLoader는 html 파일 안에 내용을 읽어옵니다.
+
+class UnstructedHTMLLoader:
+    def __init__(self, path):
+        pass
+    def load():
+        pass
